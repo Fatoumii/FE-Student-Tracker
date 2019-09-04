f04721dd008dbf9b74ea02f40133f79c78a87381
Fatimah <fatimah@Fatimahs-MBP.broadband>
2019-09-04 14:56:33 +0100

    set loading

--- front-end-practise/src/components/Main.js
+++ front-end-practise/src/components/Main.js
@@ -3,30 +3,39 @@ import * as api from "../utils";
 
 class Main extends React.Component {
   state = {
-    students: []
+    students: [],
+    loading: true
   };
   render() {
-    const { students } = this.state;
+    const { students, loading } = this.state;
 
     return (
       <div>
-        {students.map(student => {
-          return (
-            <ul className="studentList" key={student.id}>
-              <li>
-                {student.name.includes(" ") ? student.name.toUpperCase() : null}
-              </li>
-            </ul>
-          );
-        })}
+        {loading === true ? (
+          "Loading..."
+        ) : (
+          <div>
+            <h2>Students</h2>
+            {students.map(student => {
+              return (
+                <ul className="studentList" key={student.id}>
+                  <li>
+                    {student.name.includes(" ")
+                      ? student.name.toUpperCase()
+                      : null}
+                  </li>
+                </ul>
+              );
+            })}
+          </div>
+        )}
       </div>
     );
   }
   componentDidMount = () => {
     api.getStudent().then(data => {
-      this.setState({ students: data });
+      this.setState({ students: data, loading: false });
     });
   };
 }
-
 export default Main;
