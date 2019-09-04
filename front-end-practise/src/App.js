878f75a23d695b2ab4e1214a08511472c8e864b4
Fatimah <fatimah@Fatimahs-MBP.broadband>
2019-09-04 12:21:31 +0100

    added basic routes

--- front-end-practise/src/App.js
+++ front-end-practise/src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Router } from "@reach/router";
 import "./App.css";
+import Home from "./components/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Content from "./components/Content";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <Header />
+      <Router>
+        <Home path="/" />
+        <Content path="/content" />
+      </Router>
+      <Footer />
     </div>
   );
 }
