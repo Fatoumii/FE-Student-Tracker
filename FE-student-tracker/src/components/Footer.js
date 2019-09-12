255f21bd87359f4c130b15cef77ec6c7ee29d323
Fatimah <fatimah@Fatimahs-MBP.broadband>
2019-09-12 11:29:49 +0100

    added whitespace in footer and ensured it was desired color

--- FE-student-tracker/src/components/Footer.js
+++ FE-student-tracker/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer">
-      You can view some of my other projects here >>
+      You can view some of my other projects here >>&nbsp;&nbsp;
       <a href="https://github.com/Fatoumii"> https://github.com/Fatoumii</a>
     </footer>
   );
