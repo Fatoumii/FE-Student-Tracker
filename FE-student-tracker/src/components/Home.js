79ccf6748f187aa4ba4a5c4652fba235d76875a1
Fatimah <fatimah@Fatimahs-MBP.broadband>
2019-09-20 13:10:59 +0100

    completed home page and media queries for it

--- FE-student-tracker/src/components/Home.js
+++ FE-student-tracker/src/components/Home.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Pic from "../home.jpeg";
+import { Link } from "@reach/router";
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <img src={Pic} alt="project phase celebrations" />
+      <div class="hometext">
+        <h3>Enter the site to view students and ammend their status</h3>
+      </div>
+      <button className="enter">
+        <a href="/students">Enter</a>
+      </button>
+    </div>
+  );
 };
 
 export default Home;
